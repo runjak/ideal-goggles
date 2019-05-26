62031eb904fbc21c5d879f436ac269dc4559e7fc
Jakob Runge <sicarius@g4t3.de>
2019-05-26 12:04:39 +0200

    towards currency printing
    - currently getting an U_MISSING_RESOURCE_ERROR

--- src/main.c
+++ src/main.c
@@ -1,19 +1,50 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 // https://ssl.icu-project.org/apiref/icu4c/
 #include <unicode/uloc.h>
+#include <unicode/ucurr.h>
+#include <unicode/ucnv.h>
 
-int main(int argc, char const *argv[])
-{
-  printf("Hello locales!\n");
-
+int main(int argc, char const *argv[]) {
   int32_t localeCount = uloc_countAvailable();
+  size_t currencyLength = sizeof(UChar) * 3;
+  UChar* currency = (UChar*) malloc(currencyLength);
+  UErrorCode ec = U_ZERO_ERROR;
+  UConverter* conv = ucnv_open("ASCII", &ec);
+
+  if (ec != U_ZERO_ERROR) {
+    printf("Error in ucnv_open: %s\n", u_errorName(ec));
+    return ec;
+  }
 
   printf("Available locales: %i\n", localeCount);
 
   for(int32_t i = 0; i < localeCount; i++) {
-    printf("Got locale: %s\n", uloc_getAvailable(i));
+    const char* locale = uloc_getAvailable(i);
+    printf("Got locale: %s\n", locale);
+
+    ucurr_forLocale(locale, currency, currencyLength, &ec);
+    if (ec != U_ZERO_ERROR) {
+      printf("Error in ucurr_forLocale: %s\n", u_errorName(ec));
+      continue;
+    }
+
+    int32_t currencyAsStringLength = UCNV_GET_MAX_BYTES_FOR_STRING(currencyLength, ucnv_getMaxCharSize(conv));
+    char* currencyAsString = malloc(currencyAsStringLength);
+    ucnv_fromUChars(conv, currencyAsString, currencyAsStringLength, currency, currencyLength, &ec);
+
+    printf("got currency: %s\n", currencyAsString);
+
+    free(currencyAsString);
   }
+
+  // unumf_openForSkeletonAndLocale
+  // UNumberFormatStyle
+  // unum_formatDoubleCurrency
+  // ucurr_forLocale
+
+  free(currency);
 
   return 0;
 }
