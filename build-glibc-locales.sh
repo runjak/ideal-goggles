2725d70998d7cc5fd1b59db145aff8d510879802
Jakob Runge <sicarius@g4t3.de>
2019-05-31 15:12:10 +0200

    improve build-glibc-locales.sh

--- build-glibc-locales.sh
+++ build-glibc-locales.sh
@@ -6,5 +6,5 @@ locales=$(find glibc-install/share/i18n/locales -type f | cut -d '/' -f5-)
 for locale in $locales
 do
   echo "localedef for ${locale}"
-  ./glibc-install/bin/localedef -i ${locale} -c -f UTF-8 -A ./glibc-install/share/locale/locale.alias ${locale}.UTF-8
+  ./glibc-install/bin/localedef -i ${locale} -c -f UTF-8 -A ./glibc-install/share/locale/locale.alias ${locale}.UTF-8 || true
 done
